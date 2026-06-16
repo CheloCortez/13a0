6a8ae73e51c4a56c4c89fa44320c63c95307edc9
Marcelo Monteiro <mmonteiro@lemontech.com.br>
2026-06-16 17:07:52 -0300

    feat: adiciona Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { base } from '$app/paths';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
